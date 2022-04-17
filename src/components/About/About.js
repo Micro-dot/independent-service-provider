48d20b1ef95d0efec166b114582f89b7738b3cc8
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-04-17 08:30:22 +0600

    set about components

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import mahfuz from '../../images/about/10861 A.JPG';
+import './About.css';
 const About = () => {
     return (
         <div>
-            <h1>This is About Part</h1>
+            <div className='picture'>
+                <img src={mahfuz} alt="" />
+            </div>
+            <div>
+                <h1>Hey. What's Up?</h1>
+                <p>
+                    I am Mahfuzur Rahman - a junior web developer and learner of Programming Hero. Develop unknown information using acquired knowledge. Hold on like glue until the unfinished work is finish. Achieve new knowledge in new days. To fill one's own deficit. Finally, build myself at the top of the full stack web development.
+                </p>
+            </div>
         </div>
     );
 };
