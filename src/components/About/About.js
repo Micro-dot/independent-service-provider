7a5a77b1978187f92aedc0fa86fe28cb33c0ea03
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-04-17 08:47:12 +0600

    about page add this website

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,14 +4,17 @@ import './About.css';
 const About = () => {
     return (
         <div>
-            <div className='picture'>
-                <img src={mahfuz} alt="" />
-            </div>
-            <div>
-                <h1>Hey. What's Up?</h1>
-                <p>
-                    I am Mahfuzur Rahman - a junior web developer and learner of Programming Hero. Develop unknown information using acquired knowledge. Hold on like glue until the unfinished work is finish. Achieve new knowledge in new days. To fill one's own deficit. Finally, build myself at the top of the full stack web development.
-                </p>
+            <div className='d-flex align-items-center container mt-5 '>
+                <div className='picture me-5' >
+                    <img src={mahfuz} alt="" />
+                </div>
+                <div className='ms-5'>
+                    <h1>Hey. What's Up?</h1>
+                    <p className='text-start fs-5'>
+                        I am Mahfuzur Rahman - a junior web developer and learner of Programming Hero. Develop unknown information using acquired knowledge. Hold on like glue until the unfinished work is finish. Achieve new knowledge in new days. To fill one's own deficit. Finally, build myself at the top of the full stack web development.
+                    </p>
+                </div>
+                
             </div>
         </div>
     );
