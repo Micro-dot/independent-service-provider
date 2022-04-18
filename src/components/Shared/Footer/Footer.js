4159594b2afdd55fb950891bef332d3c004901ff
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-04-18 20:50:31 +0600

    footer feature added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,47 @@
 import React from 'react';
-
+import './Footer.css';
+import facebook from '../../../images/facebook.png';
+import youtube from '../../../images/youtube.png';
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div>
-           <p>Copyright © {year} </p>
+        <div class="footer-clean">
+            <footer>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>Services</h3>
+                            <ul>
+                                <li>Crossfit Training</li>
+                                <li>Cardio Training</li>
+                                <li>Endurance Training</li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>About</h3>
+                            <ul>
+                                <li> Company </li>
+                                <li> Team </li>
+                                <li> exercise advice </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>Careers</h3>
+                            <ul>
+                                <li> Bodybuilding coach </li>
+                                <li> Fitness apparel designer </li>
+                                <li> Exercise physiologist. </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 social-item">
+                            <img src={facebook} alt="" />
+                            <img src={youtube} alt="" />
+                            <p class="copyright">Big Gym Copyright © {year}</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/components/Shared/Service/Service.js
+++ src/components/Shared/Service/Service.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Card} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './Service.css';
+import './Service.css';
 const Service = ({ service }) => {
     const { title, name, img, description, price } = service;
     const navigate = useNavigate();
@@ -12,18 +13,18 @@ const Service = ({ service }) => {
 
     return (
         <div className='container'>
-            <div className='shadow-sm p-2 rounded'>
+            <div className='shadow-sm p-2 rounded main-card'>
                     <div className="card-body">
                         <h1 className="fw-bold">{title}</h1>
                         <h2>{name}</h2>
-                        <h3 className="fw-bold">${price}</h3>
-                        <Card.Img className='img-fluid w-100 mg-50' variant="top" src={img} />
-                        <div className="">
+                        <h3 className="fw-bold text-warning">${price}</h3>
+                        <Card.Img className='images' variant="top" src={img} />
+                        <div className="description">
                             {description}
                         </div>
                     </div>
                     <div className="d-flex justify-content-center">
-                        <Button onClick={()=>navigateChoosePlan({service})} className="btn w-75 bg-dark text-info rounded-pill">Choose plan</Button>
+                        <Button onClick={()=>navigateChoosePlan({service})} className=" choose-button btn w-75 bg-dark text-info rounded-pill ">Choose plan</Button>
                     </div>
             </div>
         </div >
